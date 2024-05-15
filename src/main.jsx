1ac027c76b5f8000c48410842279256260c13454
kscold <chan6502@gmail.com>
2024-05-15 23:29:03 +0900

    PWA 빌드를 테스트하기 위한 배포

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -7,4 +8,28 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+=======
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import "./index.css"
+import store from "./store/index.js"
+import { Provider } from "react-redux"
+import { register } from "./service-worker"
+// import {PersistGate} from 'redux-persist/integration/react';
+// import {persistStore} from 'redux-persist';
+
+//export let persistor = persistStore(store)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      {/* <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate> */}
+      <App />
+    </Provider>
+  </React.StrictMode>
+>>>>>>> Stashed changes
 )
+
+register()
